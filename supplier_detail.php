9ed7c01eab1d8a966a6607dc13e498d40e724359
zanra2401 <grubgrex@gmail.com>
2025-11-06 18:52:10 +0700

    update

--- supplier_detail.php
+++ supplier_detail.php
@@ -38,7 +38,38 @@
 
     <!-- Page Content  -->
     <div id="content" class="p-4 p-md-5 pt-5" style="overflow: auto; height: 100vh;">
+        <form action="./proses/supplier/update.php" method="POST">
 
+                <div class="row g-3">
+
+                    <input type="hidden" id="id" name="id" value=<?= isset($_GET["id"]) ? $_GET["id"] : "" ?> >
+
+                    <div class="col-6">
+                        <label for="nama" class="form-label">Nama</label>
+                        <input style="border: 1px solid rgb(206, 212, 218);" type="text" name="nama" value="<?= $supplier["nama"] ?>" class="form-control" id="nama"
+                        required>
+                    </div>
+                    <div class="col-6">
+                        <label for="nama" class="form-label">Telp</label>
+                        <input style="border: 1px solid rgb(206, 212, 218);" type="text" name="telp" value="<?= $supplier["telp"] ?>" class="form-control" id="telp"
+                        required>
+                    </div>
+                    <div class="col-12">
+                        <label for="nama" class="form-label">Telp</label>
+                        <textarea class="form-control" style="border: 1px solid rgb(206, 212, 218);" name="alamat" id="alamat"><?= $supplier["alamat"] ?></textarea>
+                    </div>
+                </div>
+
+                <div style="width: 100%; display: flex; gap: 10px; justify-content: flex-end; margin-top: 20px;">
+                    <a type="button" class="btn btn-danger" href=<?= isset($pelanggan) ? "/MasterDetail/proses/transaksi/delete.php?delete=true&id={$_GET["id"]}" : "#" ?>>
+                        Hapus
+                    </a>
+                    <a type="button" class="btn btn-secondary" href="/MasterDetail/supplier.php">
+                        Batal
+                    </a>
+                    <button type="submit" class="btn btn-primary" name="submit">Simpan</button>
+                </div>
+            </form>
     </div>
 </body>
 </html>
